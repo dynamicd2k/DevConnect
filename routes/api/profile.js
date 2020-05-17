6e4e9a3e6230a10cab9e2c2b66ff7833e45f6b92
deepakd2k <srivastav.deepak@hotmail.com>
2020-05-17 17:33:35 +0530

    Profile commit-1

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,10 +1,109 @@
 const express = require('express');
-
+const { check, validationResult } = require('express-validator');
 const router = express.Router();
+const auth = require('../../middleware/auth');
+const Profile = require('../../model/Profile');
 
-//@route    GET api/profile
-//@desc     Test route
-//@access   Public
-router.get('/', (req, res) => res.send('Profile route'));
+//@route    GET profile/me
+//@desc     Get current user profile
+//@access   Private
+router.get('/me', auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({
+      user: req.user.id,
+    }).populate('user', ['name', 'avatar']);
+    if (!profile) {
+      return res.status(400).json({ msg: 'There is no profile for this user' });
+    }
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
 
+//@route    POST profile
+//@desc     Create or update user profile
+//@access   Private
+
+router.post(
+  '/',
+  auth,
+  [
+    check('jobrole', 'Job role is required').not().isEmpty(),
+    check('skills', 'Skills are required').not().isEmpty(),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const {
+      company,
+      website,
+      location,
+      bio,
+      jobrole,
+      githubusername,
+      skills,
+      youtube,
+      facebook,
+      twitter,
+      instagram,
+      linkedin,
+    } = req.body;
+
+    //build profile object
+
+    const profilefields = {};
+
+    profilefields.user = req.user.id;
+    if (company) profilefields.company = company;
+    if (website) profilefields.website = website;
+    if (location) profilefields.location = location;
+    if (bio) profilefields.bio = bio;
+    if (jobrole) profilefields.jobrole = jobrole;
+    if (githubusername) profilefields.githubusername = githubusername;
+
+    if (skills)
+      profilefields.skills = skills.split(',').map((skills) => skills.trim());
+
+    //Build social object
+    profilefields.social = {};
+    if (youtube) profilefields.social.youtube = youtube;
+    if (facebook) profilefields.social.facebook = facebook;
+    if (twitter) profilefields.social.twitter = twitter;
+    if (instagram) profilefields.social.instagram = instagram;
+    if (linkedin) profilefields.social.linkedin = linkedin;
+
+    try {
+      let profile = await Profile.findOne({ user: req.user.id });
+
+      if (profile) {
+        //Update profile
+
+        profile = await Profile.findOneAndUpdate(
+          { user: req.user.id },
+          { $set: profilefields },
+          { new: true }
+        );
+
+        return res.json(profile);
+      }
+
+      //Create
+
+      profile = new Profile(profilefields);
+      await profile.save();
+
+      res.json(profile);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+    res.send('Hello');
+  }
+);
 module.exports = router;
