ef3dd5b852133805fae1891a05db3aa5f4ab27a7
deepakd2k <srivastav.deepak@hotmail.com>
2020-05-22 20:46:39 +0530

    Front end html and css setup commit

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react';
+import Navbar from './components/layout/Navbar.js';
+import Landing from './components/layout/Landing.js';
+import './App.css';
+
+const App = () => (
+  <Fragment className='App'>
+    <Navbar></Navbar>
+    <Landing></Landing>
+  </Fragment>
+);
+
+export default App;
